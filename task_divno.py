722f9b573ae98080d0d486ec29b2cda3c31fa65e
Monikaa24 <monidayalan1629@gmail.com>
2024-04-15 22:37:14 +0530

    code for sumor avg

--- task_divno.py
+++ task_divno.py
@@ -1,7 +1,44 @@
-num1=int(input("enter num1 : "))
-num2=int(input("enter num2 : "))
-num3=int(input("enter num3 : "))
+######num1=int(input("enter num1 : "))
+######num2=int(input("enter num2 : "))
+######num3=int(input("enter num3 : "))
+######
+######for i in range (1,num3+1):
+######    if (i%num1==0 and i%num2==0):
+##        print(i)
 
-for i in range (1,num3+1):
-    if (i%num1==0 and i%num2==0):
-        print(i)
+
+
+
+
+num=int(input("enter the num : "))
+l=0
+for i in range (1,num+1):
+    a=int(input())
+    l+=a
+if input()=="sum":
+      print(l)
+else:
+     print((l)/num)
+
+
+
+
+
+
+
+
+
+
+##a=int(input("Enter the number of terms: "))
+##k=0
+##print("Enter",a,"terms: ")
+##for i in range(1,a+1):
+##    ai=int(input())
+##    k+=ai
+##z=input("Enter 'A' if you want to get the Sum or Enter 'B' if you want to get the Average:")
+##if z=='A':
+##    print("The Sum:",k)
+##elif z=='B':
+##    print("The Average:",k/a)
+##else:
+##    print("Invalid Option")
---
+++ tasksum_avg.py
@@ -0,0 +1,11 @@
+num=int(input("enter the num : "))
+l=0
+for i in range (1,num+1):
+    a=int(input())
+    l+=a
+if input()=="sum":
+      print(l)
+else:
+     print((l)/num)
+
+
