084f6cebfed726f0e3ff2298e3727da127fb8a71
Monikaa24 <monidayalan1629@gmail.com>
2024-04-22 20:31:12 +0530

    code for area

---
+++ main.py
@@ -0,0 +1,7 @@
+print("FINDING AREA AND PERIMETER")
+length=float(input("enter the length :"))
+breadth=float(input("enter the breadth :"))
+area=length*breadth
+print("area of rectangle :",area)
+perimeter=2*(length+breadth)
+print("perimeter of rectangle :",perimeter)
